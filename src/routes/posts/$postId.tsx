d342e6cc68bc84d2b03a0ab2efbda2ec26423869
quangdd <quangm202@gmail.com>
2025-02-22 00:15:01 +0700

    update design of main posts browsing page

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,223 +1,146 @@
-import { createFileRoute } from '@tanstack/react-router';
-import {
-  IconArrowBarToLeft,
-  IconCarambola,
-  IconCaretLeft,
-  IconCaretRight,
-  IconChevronCompactDown,
-  IconFoldDown,
-  IconHeart,
-  IconMinus,
-  IconPlus,
-  IconStar,
-  IconToggleRight,
-  IconWindowMinimize,
-  IconX,
-} from '@tabler/icons-react';
+import { createFileRoute, Link, useParams } from '@tanstack/react-router';
+import { IconFoldDown, IconWindowMinimize, IconX } from '@tabler/icons-react';
+import React, { ComponentRef, useCallback, useEffect, useRef, useState } from 'react';
+import PrismaZoom from 'react-prismazoom';
 
 export const Route = createFileRoute('/posts/$postId')({
   component: RouteComponent,
 });
 
+interface Post {
+  id: string;
+  author: string;
+  download_url: string;
+  height: number;
+  width: number;
+}
+
 function RouteComponent() {
+  const { postId } = useParams({ from: '/posts/$postId' });
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const prismaZoom = useRef<ComponentRef<typeof PrismaZoom>>(null);
+  const zoomCounterRef = useRef<HTMLSpanElement>(null);
+  const [allowZoom, setAllowZoom] = useState(true);
+
+  useEffect(() => {
+    fetch('https://picsum.photos/v2/list')
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  useEffect(() => {
+    if (postId) {
+      const foundPost = posts.find((p) => p.id === postId);
+      setSelectedPost(foundPost || null);
+    }
+  }, [postId, posts]);
+
+  const onZoomChange = useCallback((zoom: number) => {
+    if (!zoomCounterRef.current) return;
+    zoomCounterRef.current.innerText = `${Math.round(zoom * 100)}%`;
+  }, []);
+
+  const onClickOnZoomOut = () => {
+    if (allowZoom) {
+      prismaZoom.current?.zoomOut(1);
+    }
+  };
+
+  const onClickOnZoomIn = () => {
+    if (allowZoom) {
+      prismaZoom.current?.zoomIn(1);
+    }
+  };
+
+  const onClickOnLock = () => {
+    setAllowZoom((allowZoom) => !allowZoom);
+  };
+
   return (
     <div className="slide-out-wrapper">
       <div className="slide-out-inner">
-        <div className="slide-out-trigger-actions">
-          <button className="slide-out-trigger-btn" type="button">
-            <span className="sr-only">Hide slide-out modal</span>
-            <IconFoldDown />
-          </button>
-          <button className="slide-out-trigger-btn" type="button">
-            <span className="sr-only">Close slide-out modal</span>
-            <IconWindowMinimize />
-          </button>
-          <button className="slide-out-trigger-btn" type="button">
-            <span className="sr-only">Close slide-out modal</span>
-            <IconX />
-          </button>
-        </div>
+        <div className={`slide-out-content overflow-x-hidden overflow-y-scroll`}>
+          {/* Post Media Container */}
+          {selectedPost && (
+            <div className="post-media-container bg-surface-a50 flex h-full w-full items-center justify-center overflow-hidden">
+              <div className="post-media-zoom-indicator bg-surface-a10 absolute bottom-1 z-50 flex items-center justify-center rounded-2xl p-2 text-white opacity-50 hover:opacity-100">
+                <button
+                  type={'button'}
+                  className="h-6 w-6 cursor-pointer"
+                  onClick={onClickOnZoomOut}
+                  disabled={!allowZoom}
+                >
+                  <span className="sr-only">Zoom out</span>
+                  <span>-</span>
+                </button>
 
-        <div className="slide-out-content">
-          {/*<!-- Zoom Controller (Right) -->*/}
-          <div className="zoom-controller-bar">
-            <button className="zoom-btn zoom-in" type="button">
-              <span className="sr-only">Zoom in</span>
-              <IconPlus />
-            </button>
-            <label className="sr-only" htmlFor="zoom-slider">
-              Zoom
-            </label>
-            <input
-              className="zoom-slider"
-              id="zoom-slider"
-              max="150"
-              min="50"
-              type="range"
-              value="100"
-              onChange={($event) => console.log($event.target.value)}
-            />
-            <button className="zoom-btn zoom-out" type="button">
-              <span className="sr-only">Zoom out</span>
-              <IconMinus />
-            </button>
-          </div>
+                <span className="post-media-zoom-label" ref={zoomCounterRef}>
+                  100%
+                </span>
 
-          {/*<!-- Gallery Slider (Up from bottom with trigger button) -->*/}
-          <div className="gallery-slider">
-            <button type="button" className="gallery-toggle-btn">
-              <span></span>
-              <IconChevronCompactDown />
-            </button>
-            <button type="button" className="gallery-nav prev">
-              <span className="sr-only">Slider scroll left</span>
-              <IconCaretLeft />
-            </button>
-            <div className="gallery-slider-list">
-              {/*<!-- Placeholder with divs if 200 x 200 px-->*/}
-              <img
-                className="thumbnail active"
-                src="https://placehold.co/200x200/000000/FFFF00"
-                alt="Image 1"
-              />
-              <img
-                className="thumbnail"
-                src="https://placehold.co/200x200/000000/FFF"
-                alt="Image 2"
-              />
-              <img
-                className="thumbnail"
-                src="https://placehold.co/200x200/000000/00A36C"
-                alt="Image 3"
-              />
-            </div>
-            <button type="button" className="gallery-nav next">
-              <span className="sr-only">Slider scroll right</span>
-              <IconCaretRight />
-            </button>
-          </div>
-        </div>
+                <button
+                  type={'button'}
+                  className="h-6 w-6 cursor-pointer"
+                  onClick={onClickOnZoomIn}
+                  disabled={!allowZoom}
+                >
+                  <span className="sr-only">Zoom in</span>
+                  <span>+</span>
+                </button>
 
-        <div className="sidebar">
-          <div className="sidebar-content">
-            <div className="sidebar-actions">
-              <button type="button">
-                <span className="sr-only">Move sidebar to the left</span>
-                <IconArrowBarToLeft />
-              </button>
-              <button className="minimize-sidebar" type="button">
-                <span className="sr-only">Minimize sidebar</span>
-                <IconToggleRight />
-              </button>
-            </div>
-
-            <div className="post-info">
-              <div className="section-title">DETAILS</div>
-              <div className="list-items">
-                <div className="item">
-                  <div className="item-label">
-                    <span>Post ID</span>
-                  </div>
-                  <div className="item-value">
-                    <span>1234567890</span>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="item-label">
-                    <span>File Type</span>
-                  </div>
-                  <div className="item-value">
-                    <span>image/jpg</span>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="item-label">
-                    <span>File size</span>
-                  </div>
-                  <div className="item-value">
-                    <span>2.02 MB</span>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="item-label">
-                    <span>Uploader</span>
-                  </div>
-                  <div className="item-value">
-                    <a href="#">quangdd1202</a>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="item-label">
-                    <span>Uploaded at</span>
-                  </div>
-                  <div className="item-value">
-                    <span>Feb 10, 2025</span>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="item-label">
-                    <span>Rating</span>
-                  </div>
-                  <div className="item-value">
-                    <span>G</span>
-                  </div>
-                </div>
+                <button type={'button'} className="h-6 w-6 cursor-pointer" onClick={onClickOnLock}>
+                  <svg className={allowZoom ? 'fill-current' : 'fill-red-500'} viewBox="0 0 24 24">
+                    {!allowZoom ? (
+                      <path d="M12 13a1.49 1.49 0 0 0-1 2.61V17a1 1 0 0 0 2 0v-1.39A1.49 1.49 0 0 0 12 13zm5-4V7A5 5 0 0 0 7 7v2a3 3 0 0 0-3 3v7a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3zM9 7a3 3 0 0 1 6 0v2H9zm9 12a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1z" />
+                    ) : (
+                      <path d="M12 13a1.49 1.49 0 0 0-1 2.61V17a1 1 0 0 0 2 0v-1.39A1.49 1.49 0 0 0 12 13zm5-4H9V7a3 3 0 0 1 5.12-2.13 3.08 3.08 0 0 1 .78 1.38 1 1 0 1 0 1.94-.5 5.09 5.09 0 0 0-1.31-2.29A5 5 0 0 0 7 7v2a3 3 0 0 0-3 3v7a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3zm1 10a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1z" />
+                    )}
+                  </svg>
+                </button>
               </div>
 
-              <div className="post-list-tags">
-                <div className="section-title">TAGS</div>
-                <div className="list-items">
-                  <div className="item">
-                    <div className="flex">
-                      <button className="tag-follow" type="button">
-                        <span className="sr-only">Follow tag</span>
-                        <IconCarambola />
-                      </button>
-                      <button className="tag-pill" type="button">
-                        <span className="tag-name">
-                          Blue Archive Blue Archive Blue Archive Blue Archive Blue Archive
-                        </span>
-                      </button>
-                    </div>
-                    <a className="tag-post-count" href="#">
-                      <span> 2002</span>
-                    </a>
-                  </div>
-                  <div className="item">
-                    <button className="tag-follow" type="button">
-                      <span className="sr-only">Unfollow tag</span>
-                      <IconCarambola />
-                    </button>
-                    <button className="tag-pill" type="button">
-                      <span className="tag-name">
-                        Blue Archive Blue Archive Blue Archive Blue Archive Blue Archive
-                      </span>
-                    </button>
-                    <a className="tag-post-count" href="#">
-                      <span> 2002</span>
-                    </a>
-                  </div>
-                </div>
+              <div className="post-media-controls">
+                <button className="slide-out-trigger-btn" type="button">
+                  <span className="sr-only">Hide slide-out modal</span>
+                  <IconFoldDown />
+                </button>
+                <button className="slide-out-trigger-btn" type="button">
+                  <span className="sr-only">Minimize slide-out modal</span>
+                  <IconWindowMinimize />
+                </button>
+                <button className="slide-out-trigger-btn" type="button">
+                  <span className="sr-only">Close slide-out modal</span>
+                  <IconX />
+                </button>
               </div>
-            </div>
-          </div>
 
-          <div className="sidebar-bottom">
-            <div className="post-actions">
-              <button className="post-like-btn" type="button">
-                <span className="sr-only">Like post</span>
-                <IconHeart />
-              </button>
-              <div className="post-vote">
-                <IconStar />
-                <IconStar />
-                <IconStar />
-                <IconStar />
-                <IconStar />
+              {!allowZoom && <div className={`overlay absolute inset-0 z-10`}></div>}
+
+              <div className="post-media">
+                <PrismaZoom ref={prismaZoom} onZoomChange={onZoomChange} allowZoom={allowZoom}>
+                  <img src={selectedPost.download_url} alt="Selected Post" />
+                </PrismaZoom>
               </div>
             </div>
+          )}
+
+          {/* Posts List */}
+          <div className="posts-list-container flex flex-wrap justify-evenly">
+            {posts.map((post) => (
+              <div
+                key={post.id}
+                className="flex h-[300px] w-[320px] items-center justify-center p-4"
+              >
+                <Link from="/posts" to={post.id}>
+                  <img className="max-h-64" src={post.download_url} alt={`Post ${post.id}`} />
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
+
+        <div className="sidebar"></div>
       </div>
     </div>
   );
--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -1,29 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { useState } from 'react';
-import SlideOut from '../../components/SlideOut';
 
 export const Route = createFileRoute('/posts/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
-
   return (
     <div>
       <h1>Posts List</h1>
-      <ul>
-        {['post1', 'post2', 'post3'].map((postId) => (
-          <li key={postId}>
-            <button onClick={() => setSelectedPostId(postId)}>Open {postId}</button>
+      {['post1', 'post2', 'post3'].map((postId) => (
+        <ul>
+          <li>
+            <Link key={postId} from={'/posts'} to={postId}>
+              Post {postId}
+            </Link>
           </li>
-        ))}
-      </ul>
-
-      {/* Slide-Out Component */}
-      {selectedPostId && (
-        <SlideOut postId={selectedPostId} onClose={() => setSelectedPostId(null)} />
-      )}
+        </ul>
+      ))}
     </div>
   );
 }
